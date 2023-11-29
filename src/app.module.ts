626957753d7c43bf106a8b838e539e1d46137855
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-11-29 17:48:12 +0000

    feat: init user post route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,41 @@
+import {
+  ConflictException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { user } = this.prisma
+    const { name, email, password } = body
+
+    const userWithSameEmail = await user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with same  e-mail already exists.')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
