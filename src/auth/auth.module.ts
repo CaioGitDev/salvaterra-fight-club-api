fb60c128b2766ec52d6755ce9cd9847ec343b2f0
Caio Rosa <dev.caiorosa@gmail.com>
2023-11-29 23:23:39 +0000

    feat: configure auth with JTW and passport

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+import { Env } from 'src/env'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      async useFactory(config: ConfigService<Env, true>) {
+        const secret = config.get('JWT_SECRET', { infer: true })
+        return {
+          secret,
+        }
+      },
+    }),
+  ],
+})
+export class AuthModule {}
