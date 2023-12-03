25b94aa468a725c17256f6ce56adb62a78f2d6b1
Caio Rosa <dev.caiorosa@gmail.com>
2023-12-03 20:29:32 +0000

    test: Add E2E test for creating an account

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,40 @@
-test('1 + 1 = 2', () => {
-  expect(1 + 1).toBe(2)
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Create account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'Test',
+      email: 'test@gmail.com',
+      password: '123456',
+    })
+
+    expect(response.status).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'test@gmail.com',
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
 })
