76ba66b5d00a857a74707cc67a410b7e3af89409
Caio Rosa <dev.caiorosa@gmail.com>
2023-12-10 22:00:31 +0000

    Add PUT member guardian controller and e2e test

---
+++ src/infra/http/controllers/put-member-guardian.controller.ts
@@ -0,0 +1,75 @@
+import { Body, Controller, Put, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { TokenPayload } from '@/infra/auth/jwt.strategy'
+import { z } from 'zod'
+
+const putMemberGuardianBodySchema = z.object({
+  memberId: z.string().uuid(),
+  fullName: z.string().optional(),
+  relationshipDegreeId: z.number().optional(),
+  contact: z.string().optional(),
+  address: z.string().optional(),
+  city: z.string().optional(),
+  county: z.string().optional(),
+  parish: z.string().optional(),
+  postalCode: z.string().optional(),
+})
+
+type PutMemberGuardianBodySchema = z.infer<typeof putMemberGuardianBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(putMemberGuardianBodySchema)
+
+@Controller('/member/guardian')
+@UseGuards(JwtAuthGuard)
+export class PutMemberGuardianController {
+  constructor(private prisma: PrismaService) {}
+
+  @Put()
+  async handle(
+    @Body(bodyValidationPipe) body: PutMemberGuardianBodySchema,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    const { sub: userId } = user
+
+    const {
+      memberId,
+      fullName,
+      address,
+      contact,
+      city,
+      county,
+      parish,
+      postalCode,
+      relationshipDegreeId,
+    } = body
+
+    const guardianExists = await this.prisma.guardian.findFirst({
+      where: {
+        memberId,
+      },
+    })
+
+    if (!guardianExists) throw new Error('Member guardian not found')
+
+    const updatedMemberGuardian = await this.prisma.guardian.update({
+      where: {
+        id: guardianExists.id,
+      },
+      data: {
+        fullName,
+        address,
+        contact,
+        city,
+        county,
+        parish,
+        postalCode,
+        relationshipDegreeId,
+      },
+    })
+
+    return updatedMemberGuardian
+  }
+}
